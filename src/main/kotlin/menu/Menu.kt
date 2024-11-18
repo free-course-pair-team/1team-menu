9aa0ceccfba2324048ffadffbcf285d2b719fcff
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-18 19:19:07 +0900

    feat(OutputView): 시작 메시지 출력 기능 구현

---
+++ src/main/kotlin/menu/Menu.kt
@@ -0,0 +1,24 @@
+package menu
+
+//- [ ] 코치 이름 입력
+//  - [ ] 이름은 최소 2글자 최대 4글자
+//  - [ ] 인원은 2명에서 5명
+
+class Menu(
+    private val inputView: InputView = InputView(),
+    private val outputVuew: OutputView = OutputView()
+) {
+
+    fun start() {
+        outputVuew.lunchRecommendMessage()
+        val coaches = inputView.readCoachNames()
+        validateCoachesName(coaches)
+    }
+
+    private fun validateCoachesName(names: List<String>) {
+        require(names.size >= 2 || names.size <= 5) { "[ERROR] 코치는 최소 2명 최대 5명으로 입력해야 합니다." }
+        names.forEach { name ->
+            require(name.length >= 2 || name.length <= 4) { "[ERROR] 코치 이름은 최소 2 글자 최대 4글자 입력해야 합니다." }
+        }
+    }
+}
