27eef1367e811d85e5e55463b20c696c9e52aa68
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-18 20:01:08 +0900

    feat(FoodCategory): 음식 종류 열거형 객체 추가

---
+++ src/main/kotlin/menu/FoodCategory.kt
@@ -0,0 +1,9 @@
+package menu
+
+enum class FoodCategory(val type: String) {
+    JAPANESE_FOOD("일식"),
+    KOREAN_FOOD("한식"),
+    CHINESE_FOOD("중식"),
+    ASIAN_FOOD("아시안"),
+    WESTERN_FOOD("양식")
+}
