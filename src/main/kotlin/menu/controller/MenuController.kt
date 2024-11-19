c0a8c43c77403f729c74e9b1f2be9c762b61a71a
DaeYoung Jeong <eodud1025@naver.com>
2024-11-19 15:46:31 +0900

    feat(MenuController): MenuController 생성

---
+++ src/main/kotlin/menu/controller/MenuController.kt
@@ -0,0 +1,16 @@
+package menu.controller
+
+import menu.view.InputView
+import menu.view.OutputView
+
+class MenuController {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+
+    private fun inputCoachName(): List<String> {
+        outputView.printServiceStart()
+        return inputView.inputCoachName().split(",")
+    }
+
+
+}
