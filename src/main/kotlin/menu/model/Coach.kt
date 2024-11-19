996c787e37729e9580bfd8727ea88a318ada55b5
DaeYoung Jeong <eodud1025@naver.com>
2024-11-19 15:57:52 +0900

    feat(Coach): 코치 이름 예외 처리

--- src/main/kotlin/menu/model/Coach.kt
+++ src/main/kotlin/menu/model/Coach.kt
@@ -1,5 +1,18 @@
 package menu.model
 
-class Coach(private val name: String, private val notEatMenu: List<String>) {
+class Coach(private val name: String) {
+
+
+    private val notEatMenu = emptyList<String>()
+
+    init {
+        validateName()
+    }
+
+    private fun validateName() {
+        require( name.length in 2..4 ) {
+            "[ERROR] 코치의 이름은 2글자 이상 4글자 이하 이여야 합니다."
+        }
+    }
 
 }
