8bfc8461ed0b3da1c44a0065507f9bb2d9d0cb12
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-20 14:18:22 +0900

    refactor(OutputView): 하드코딩 상수로 추출
    Co-Authored-By: 김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>

--- src/main/kotlin/menu/OutputView.kt
+++ src/main/kotlin/menu/OutputView.kt
@@ -1,15 +1,24 @@
 package menu
 
 class OutputView {
-
-    fun lunchRecommendMessage() = println("점심 메뉴 추천을 시작합니다.")
-    fun menuRecommendMessage() = println("메뉴 추천 결과입니다.")
-    fun printWeekend() = println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]")
-    fun printCategory(foodCategory: List<String>) = println("[ 카테고리 | ${foodCategory.joinToString(" | ")} ]")
+    fun lunchRecommendMessage() = println(LUNCH_RECOMMEND_MESSAGE)
+    fun menuRecommendMessage() = println(MENU_RECOMMEND_MESSAGE)
+    fun printWeekend() = println(WEEKEND_MESSAGE)
+    fun printCategory(foodCategory: List<String>) = println(CATEGORY_MESSAGE.format(foodCategory.joinToString(" | ")))
     fun printFoodByCoaches(coaches: List<Coach>) {
         coaches.forEach { coach ->
-            println("[ ${coach.name} | ${coach.getRecommendedFoods().joinToString(" | ")} ]")
+            println(FOOD_BY_COACHES.format(coach.name, coach.getRecommendedFoods().joinToString(" | ")))
         }
     }
-    fun completeRecommendMessage() = println("\n추천을 완료했습니다.")
+
+    fun completeRecommendMessage() = println(COMPLETE_RECOMMEND_MESSAGE)
+
+    companion object {
+        private const val LUNCH_RECOMMEND_MESSAGE = "점심 메뉴 추천을 시작합니다."
+        private const val MENU_RECOMMEND_MESSAGE = "메뉴 추천 결과입니다."
+        private const val WEEKEND_MESSAGE = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"
+        private const val CATEGORY_MESSAGE = "[ 카테고리 | %s ]"
+        private const val FOOD_BY_COACHES = "[ %s | %s ]"
+        private const val COMPLETE_RECOMMEND_MESSAGE = "\n추천을 완료했습니다."
+    }
 }
