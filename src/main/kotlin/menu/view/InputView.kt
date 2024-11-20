14f12801c259e7526faac866c54af7e8323cc597
이용인 <yongin0517@gmail.com>
2024-11-20 15:59:26 +0900

    feat(view): 메뉴 입력 메서드 구현

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -1,9 +1,16 @@
 package menu.view
 
 import camp.nextstep.edu.missionutils.Console
+import menu.util.Validator
 
 class InputView {
     fun inputName(): String{
         return Console.readLine()
     }
+
+    fun getUserUnfriendlyMenus(user:String): String{
+        println("${user}가 못 먹는 메뉴를 입력해 주세요. (예: 마파두부, 고추잡채)")
+        return  Console.readLine()
+    }
+
 }
